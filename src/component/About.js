c65b036ff79f3e5e5fec39dc21c143edd6e1612d
NaiYo6402 <dek6402.refun@gmail.com>
2021-11-11 14:00:13 +0700

    first commit22

--- src/component/About.js
+++ src/component/About.js
@@ -4,7 +4,7 @@ function About(){
     return(
         <>
         <div className="container">
-            <h1>This is About page </h1>
+            <h1>This is About page 22</h1>
         </div>
         </>
     );
